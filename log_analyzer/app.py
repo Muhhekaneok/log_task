2c93a4361f11498e27c5b034cb74cdd32abcb0b2
darkwizard <wizardrad@gmail.com>
2025-08-11 22:55:22 +0700

    add pytest

--- log_analyzer/app.py
+++ log_analyzer/app.py
@@ -1,10 +1,15 @@
 import argparse
 import json
 from tabulate import tabulate
+from datetime import datetime
 
 
-def process_files(file_paths):
+def process_files(file_paths, report_type, filter_date_str=None):
     stats = {}
+
+    filter_date = None
+    if filter_date_str:
+        filter_date = datetime.strptime(filter_date_str, '%Y-%m-%d').date()
 
     for file_path in file_paths:
         try:
@@ -12,6 +17,14 @@ def process_files(file_paths):
                 for line in file:
                     try:
                         log_data = json.loads(line)
+
+                        if filter_date:
+                            log_date_str = log_data.get("@timestamp", "").split("T")[0]
+                            log_date = datetime.strptime(log_date_str, "%Y-%m-%d").date()
+
+                            if log_date != filter_date:
+                                continue
+
                         url = log_data["url"]
                         time = log_data["response_time"]
 
@@ -29,28 +42,25 @@ def process_files(file_paths):
             continue
 
     report_data = []
-    for url, data in stats.items():
-        count = data["count"]
-        total_time = data["total_time"]
-        avg_time = total_time / count
+    if report_type == "average":
+        for url, data in stats.items():
+            count = data["count"]
+            total_time = data["total_time"]
+            avg_time = total_time / count if count else 0
 
-        report_data.append([url, count, avg_time])
+            report_data.append([url, count, avg_time])
 
     return report_data
 
 
 def main():
     parser = argparse.ArgumentParser(description="Analyzing log-files")
-    parser.add_argument(
-        "--file",
-        nargs="+",
-        required=True,
-        help="Path to one or few log-files"
-    )
-
+    parser.add_argument("--file", nargs="+", required=True, help="Path to one or few log-files")
+    parser.add_argument("--report", required=True, choices=["average"], help="Report type 'average'")
+    parser.add_argument("--date", help="Filter date")
     args = parser.parse_args()
 
-    final_report = process_files(args.file)
+    final_report = process_files(args.file, args.report, args.date)
 
     if not final_report:
         print("JSON lines not found")
@@ -61,7 +71,6 @@ def main():
     #     print(f"Endpoint: {url}, Requests: {count}, Average time: {avg_time:.3f}")
 
     headers = ["Endpoint", "Request count", "Average time"]
-
     print(tabulate(final_report, headers=headers, tablefmt="grid", floatfmt=".3f"))
 
 if __name__ == "__main__":
---
+++ tests/test_analyzer.py
@@ -0,0 +1,37 @@
+import sys
+import os
+
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, project_root)
+
+import pytest
+from log_analyzer.app import process_files
+
+
+def test_process_files_base():
+    report = process_files(file_paths=["tests/test_data.log"], report_type="average", filter_date_str=None)
+    stats_dict = {url: {"count": count, "avg_time": avg_time} for url, count, avg_time in report}
+
+    assert len(stats_dict) == 2
+    assert stats_dict["/api/users"]["count"] == 2
+    assert stats_dict["/api/posts"]["count"] == 1
+
+
+def test_date_filter():
+    report = process_files(file_paths=["tests/test_data.log"], report_type="average", filter_date_str="2025-06-22")
+    assert len(report) == 2
+
+    user_stats = None
+    for row in report:
+        if row[0] == "/api/users":
+            user_stats = row
+            break
+
+    assert user_stats is not None
+    assert user_stats[1] == 2
+
+
+def test_no_data_for_date_filter():
+    report = process_files(file_paths=["tests/test_data.log"], report_type="average", filter_date_str="2025-01-01")
+    assert len(report) == 0
+    assert report == []
